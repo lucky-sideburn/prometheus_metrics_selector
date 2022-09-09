be887a43337f27894c06d3cf19b4b54a74b6dc5b
Eugenio Marzo <eugenio.marzo@sourcesense.com>
2022-09-09 16:52:33 +0200

    fix

--- app.py
+++ app.py
@@ -5,21 +5,24 @@ import configparser
 import re
 import logging
 import os
-
+import sys
 from flask import Flask
-logging.info('Starting OCP Prometheus Selector')
+
 app = Flask(__name__)
+app.logger.info("Starting OCP Prometheus Selector", file=sys.stdout)
+
 config = configparser.ConfigParser()
 config.sections()
 config.read('/app/conf/metrics_selector.ini')
-logging.info('Read configuration from selector.ini')
+
+app.logger.info('Read configuration from selector.ini', file=sys.stdout)
 prometheus_url = config['prometheus']['url']
-logging.info(f"Prometheus URL is {prometheus_url}")
+app.logger.info(f"Prometheus URL is {prometheus_url}", file=sys.stdout)
+
 token_env = os.environ["TOKEN"]
 token = token_env.strip()
-
 regex = config['prometheus']['regex']
-logging.info(f"OCP Prometheus Metrics Selecta will use this regex {regex}")
+app.logger.info(f"OCP Prometheus Metrics Selector will use this regex {regex}")
 
 scrape_urls = []
 
@@ -30,7 +33,8 @@ def hello_world():
 @app.route('/metrics')
 def metrics():
     global_payload = ""
-    logging.info(f"Called /metrics endpoint")
+    app.logger.info(f"Called /metrics endpoint", file=sys.stdout)
+
     scrape_urls = []
     headers = {"Authorization": f"Bearer {token}"}
     payload = requests.get(f"{prometheus_url}/api/v1/targets", verify=False, headers=headers, allow_redirects=True)
@@ -38,11 +42,11 @@ def metrics():
     for target in targets['data']['activeTargets']:
       logging.info(f"Computing target {target}")
       if re.match(regex, target['scrapePool']):
-        logging.info(f">>> Selected target {target}")
+        app.logger.info(f">>> Selected target {target}", file=sys.stdout)
         scrape_urls.append(target['scrapeUrl'])
     for scrape_url in scrape_urls:
-        logging.info(f"Scraping metrics from {scrape_url}")
+        app.logger.info(f"Scraping metrics from {scrape_url}", file=sys.stdout)
         payload = requests.get(f"f{scrape_url}", verify=False, headers=headers, allow_redirects=True)
-        logging.info(f"Appending metrics from {payload.text} to the global_payload")
+        app.logger.info(f"Appending metrics from {payload.text} to the global_payload", file=sys.stdout)
         global_payload = f"{global_payload}\n1%{payload.text}"
     return global_payload
